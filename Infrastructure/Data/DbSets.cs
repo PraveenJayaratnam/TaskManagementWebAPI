93a27e20aef3c74a8bdeed748b3460563a4e471b
Praveen <107548190+PraveenJayaratnam@users.noreply.github.com>
2025-10-27 01:56:31 +0530

    refactor: Update namespace pattern (#12)
    * Namespace pattern update

--- Infrastructure/Data/DbSets.cs
+++ Infrastructure/Data/DbSets.cs
@@ -1,55 +1,54 @@
 using Domain.Entities;
 
-namespace Infrastructure.Data
+namespace Infrastructure.Data;
+
+public static class DbSets
 {
-    public static class DbSets
+    public static void ConfigureDbSets(ModelBuilder modelBuilder)
     {
-        public static void ConfigureDbSets(ModelBuilder modelBuilder)
+        modelBuilder.Entity<User>(entity =>
         {
-            modelBuilder.Entity<User>(entity =>
-            {
-                entity.HasKey(e => e.Id);
-                entity.Property(e => e.Username).IsRequired().HasMaxLength(50);
-                entity.Property(e => e.FirstName).HasMaxLength(100);
-                entity.Property(e => e.LastName).HasMaxLength(100);
-                entity.Property(e => e.PasswordHash).IsRequired().HasMaxLength(255);
-                entity.HasIndex(e => e.Username).IsUnique();
-            });
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Username).IsRequired().HasMaxLength(50);
+            entity.Property(e => e.FirstName).HasMaxLength(100);
+            entity.Property(e => e.LastName).HasMaxLength(100);
+            entity.Property(e => e.PasswordHash).IsRequired().HasMaxLength(255);
+            entity.HasIndex(e => e.Username).IsUnique();
+        });
 
-            modelBuilder.Entity<TaskItem>(entity =>
-            {
-                entity.HasKey(e => e.Id);
-                entity.Property(e => e.Title).IsRequired().HasMaxLength(200);
-                entity.Property(e => e.Description).HasMaxLength(1000);
-                entity.Property(e => e.Status).HasConversion<int>();
-                entity.Property(e => e.Priority).HasConversion<int>();
-                
-                entity.HasOne(e => e.User)
-                    .WithMany(e => e.Tasks)
-                    .HasForeignKey(e => e.UserId)
-                    .OnDelete(DeleteBehavior.Cascade);
-            });
-        }
-
-        public static void SeedData(ModelBuilder modelBuilder)
+        modelBuilder.Entity<TaskItem>(entity =>
         {
-            var adminUserId = Guid.NewGuid();
-            var currentTime = DateTime.UtcNow;
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Title).IsRequired().HasMaxLength(200);
+            entity.Property(e => e.Description).HasMaxLength(1000);
+            entity.Property(e => e.Status).HasConversion<int>();
+            entity.Property(e => e.Priority).HasConversion<int>();
+            
+            entity.HasOne(e => e.User)
+                .WithMany(e => e.Tasks)
+                .HasForeignKey(e => e.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        });
+    }
 
-            modelBuilder.Entity<User>().HasData(
-                new User
-                {
-                    Id = adminUserId,
-                    Username = "admin",
-                    FirstName = "Admin",
-                    LastName = "User",
-                    PasswordHash = BCrypt.Net.BCrypt.HashPassword("Admin123!"),
-                    CreatedAt = currentTime,
-                    IsActive = true,
-                    IsDeleted = false
-                }
-            );
-        }
+    public static void SeedData(ModelBuilder modelBuilder)
+    {
+        var adminUserId = Guid.NewGuid();
+        var currentTime = DateTime.UtcNow;
+
+        modelBuilder.Entity<User>().HasData(
+            new User
+            {
+                Id = adminUserId,
+                Username = "admin",
+                FirstName = "Admin",
+                LastName = "User",
+                PasswordHash = BCrypt.Net.BCrypt.HashPassword("Admin123!"),
+                CreatedAt = currentTime,
+                IsActive = true,
+                IsDeleted = false
+            }
+        );
     }
 }
 
--- Infrastructure/Repositories/Generic/IGenericRepository.cs
+++ Infrastructure/Repositories/Generic/IGenericRepository.cs
@@ -1,38 +1,37 @@
 using System.Linq.Expressions;
 using Domain.Entities;
 
-namespace Infrastructure.Repositories
+namespace Infrastructure.Repositories;
+
+public interface IGenericRepository<T> where T : BaseEntity
 {
-    public interface IGenericRepository<T> where T : BaseEntity
-    {
-        Task<T?> GetByIdAsync(Guid id);
-        Task<IQueryable<T>> GetAllAsync();
-        Task<IQueryable<T>> FindAsync(Expression<Func<T, bool>> predicate);
-        Task<T?> FirstOrDefaultAsync(Expression<Func<T, bool>> predicate);
-        Task<bool> ExistsAsync(Expression<Func<T, bool>> predicate);
-        Task<int> CountAsync(Expression<Func<T, bool>>? predicate = null);
+    Task<T?> GetByIdAsync(Guid id);
+    Task<IQueryable<T>> GetAllAsync();
+    Task<IQueryable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+    Task<T?> FirstOrDefaultAsync(Expression<Func<T, bool>> predicate);
+    Task<bool> ExistsAsync(Expression<Func<T, bool>> predicate);
+    Task<int> CountAsync(Expression<Func<T, bool>>? predicate = null);
 
-        Task<IQueryable<T>> GetAllQueryable();
-        Task<IQueryable<T>> FindQueryable(Expression<Func<T, bool>> predicate);
-        
-        Task<IQueryable<T>> GetPaginatedAsync(Expression<Func<T, bool>> predicate, int pageNumber, int pageSize);
-        Task<IQueryable<T>> GetPaginatedAsync(Expression<Func<T, bool>> predicate, int pageNumber, int pageSize, Expression<Func<T, object>> orderBy);
-        Task<IQueryable<T>> GetPaginatedAsync(Expression<Func<T, bool>> predicate, int pageNumber, int pageSize, Expression<Func<T, object>> orderBy, bool isDescending);
-        
-        Task<IQueryable<T>> GetFilteredAsync(Expression<Func<T, bool>> predicate, Expression<Func<T, bool>> additionalFilter);
-        Task<IQueryable<T>> GetFilteredAsync(Expression<Func<T, bool>> predicate, Expression<Func<T, bool>> additionalFilter, Expression<Func<T, object>> orderBy);
-        Task<IQueryable<T>> GetFilteredAsync(Expression<Func<T, bool>> predicate, Expression<Func<T, bool>> additionalFilter, Expression<Func<T, object>> orderBy, bool isDescending);
+    Task<IQueryable<T>> GetAllQueryable();
+    Task<IQueryable<T>> FindQueryable(Expression<Func<T, bool>> predicate);
+    
+    Task<IQueryable<T>> GetPaginatedAsync(Expression<Func<T, bool>> predicate, int pageNumber, int pageSize);
+    Task<IQueryable<T>> GetPaginatedAsync(Expression<Func<T, bool>> predicate, int pageNumber, int pageSize, Expression<Func<T, object>> orderBy);
+    Task<IQueryable<T>> GetPaginatedAsync(Expression<Func<T, bool>> predicate, int pageNumber, int pageSize, Expression<Func<T, object>> orderBy, bool isDescending);
+    
+    Task<IQueryable<T>> GetFilteredAsync(Expression<Func<T, bool>> predicate, Expression<Func<T, bool>> additionalFilter);
+    Task<IQueryable<T>> GetFilteredAsync(Expression<Func<T, bool>> predicate, Expression<Func<T, bool>> additionalFilter, Expression<Func<T, object>> orderBy);
+    Task<IQueryable<T>> GetFilteredAsync(Expression<Func<T, bool>> predicate, Expression<Func<T, bool>> additionalFilter, Expression<Func<T, object>> orderBy, bool isDescending);
 
-        Task AddAsync(T entity);
-        Task AddRangeAsync(IEnumerable<T> entities);
-        Task UpdateAsync(T entity);
-        Task UpdateRangeAsync(IEnumerable<T> entities);
-        Task RemoveAsync(T entity);
-        Task RemoveRangeAsync(IEnumerable<T> entities);
-        Task SoftDeleteAsync(T entity);
-        Task SoftDeleteRangeAsync(IEnumerable<T> entities);
-        Task SaveChangesAsync();
+    Task AddAsync(T entity);
+    Task AddRangeAsync(IEnumerable<T> entities);
+    Task UpdateAsync(T entity);
+    Task UpdateRangeAsync(IEnumerable<T> entities);
+    Task RemoveAsync(T entity);
+    Task RemoveRangeAsync(IEnumerable<T> entities);
+    Task SoftDeleteAsync(T entity);
+    Task SoftDeleteRangeAsync(IEnumerable<T> entities);
+    Task SaveChangesAsync();
 
-    }
 }
 
